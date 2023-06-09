51fc6d4bb94890368abb5061390f4f671b683bd2
Konrad Duda <konrad.duda121@gmail.com>
2023-06-09 08:36:55 +0200

    Navbar fix

--- views/partials/header.php
+++ views/partials/header.php
@@ -12,20 +12,24 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 
-<nav class="navbar navbar-expand-lg navbar-AK bg-dark">
-    <div class="container-fluid">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/">Zadanie rekrutacyjne</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active text-light" aria-current="page" href="/">Strona główna</a>
+                    <a class="nav-link active"  href="/">Kursy walut</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active text-light" href="/calculate">Kalkulator walutowy</a>
+                    <a class="nav-link active"  href="/calculate">Kalkulator walutowy</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active text-light" href="/history">Historia przewalutowań</a>
+                    <a class="nav-link active"  href="/history">Historia przewalutowań</a>
                 </li>
             </ul>
         </div>
-    </div>
-</nav>
+        </div>
+    </nav>
